8905c873ceccf7ecaf6935b6a513f3df230aa3fe
jan Tawi Akemi <aketawi@proton.me>
2024-08-19 16:07:53 +0800

    troubleshooting, better conform to spec

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/env bash
 set -euo pipefail
 
-# Поднимает 3 примерных нода и затем запускает кластер
-# -d -- удалить ноды и кластер
+# Поднимает 3 примерных нода и затем запускает кластер.
+# ./deploy.sh -d -- удалить ноды и кластер
 
 # kubectl="kubectl"
 kubectl="minikube kubectl --"  # так как я использую minikube
 
+# check flags
 while getopts "d" option; do
   case $option in
     d) DELETE_DEPLOYMENT=true ;;
@@ -16,15 +17,17 @@ done
 
 if [[ -n ${DELETE_DEPLOYMENT:+x} ]]; then
   for f in ./node-*.yaml; do
-    $kubectl delete -f $f;
+    $kubectl delete -f $f || true  # ignore failure
   done
-  $kubectl delete -f ./test-task.yaml
+  $kubectl delete -f ./test-task.yaml || true
 
   exit 0
 fi
 
+# create nodes
 for f in ./node-*.yaml; do
   $kubectl create -f $f;
 done
 
-$kubectl create -f ./test-task.yaml
+# apply deployment
+$kubectl apply -f ./test-task.yaml
